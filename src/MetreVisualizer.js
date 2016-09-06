4410a3e705217d25fefc4a9e0faa13760311bdc3
Feynman Liang <feynman.liang@gmail.com>
2016-09-05 19:57:09 -0700

    Adds pulseColumn

---
+++ src/MetreVisualizer.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import PulseColumn from './PulseColumn';
+
+export default class MetreVisualizer extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      tempo: 100,
+    }
+  }
+
+  changeTempo(newTempo) {
+    this.setState({
+      tempo: newTempo
+    })
+  }
+
+  render() {
+    const tempo = this.state.tempo
+    const pulseColumns = [
+      {
+        name: 'Quaver',
+        crotchetMultiple: 0.5
+      },
+      {
+        name: 'Crotchet',
+        crotchetMultiple: 1
+      },
+      {
+        name: 'Minim',
+        crotchetMultiple: 2
+      },
+    ].map((entry,i) => <PulseColumn
+        key={i}
+        tempo={tempo}
+        changeTempo={::this.changeTempo}
+        {...entry}
+      />
+    );
+    return (
+      <div>
+        {pulseColumns}
+
+        <div>
+          Meter profile: TODO
+        </div>
+
+        <div>
+          Values: <input type="text" ref="value" value="TODO" disabled />
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/PulseColumn.js
@@ -0,0 +1,68 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class PulseColumn extends Component {
+  static propTypes() {
+    name: PropTypes.string.isRequired
+    tempo: PropTypes.number.isRequired
+    crotchetMultiple: PropTypes.number.isRequired
+    changeTempo: PropTypes.func.isRequired
+  };
+
+  constructor() {
+    super()
+
+    this.state = {
+      active: false
+    }
+  }
+
+  handleToggleActive(event) {
+    this.setState({
+      active: !this.state.active
+    })
+  }
+
+  handleTempoChange(event) {
+    this.props.changeTempo(this.props.crotchetMultiple * event.target.value)
+  }
+
+  handleIOIChange(event) {
+    this.props.changeTempo(this.props.crotchetMultiple * (60 / event.target.value))
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
+        <div>
+          Active:
+          <input
+            type="checkbox"
+            value={this.state.active}
+            onChange={::this.handleToggleActive}
+          />
+        </div>
+
+        <div>
+          Tempo (MM):
+          <input
+            type="text"
+            disabled={!this.state.active}
+            value={this.props.tempo * this.props.crotchetMultiple}
+            onChange={::this.handleTempoChange}
+          />
+        </div>
+
+        <div>
+          IOI (s):
+          <input
+            type="text"
+            value={60 / (this.props.tempo * this.props.crotchetMultiple)}
+            disabled={!this.state.active}
+            onChange={::this.handleIOIChange}
+          />
+        </div>
+      </div>
+    );
+  }
+}
