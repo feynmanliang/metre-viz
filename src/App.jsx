9927e74fc16f0e8b9e2fb628711816c5220563ed
Feynman Liang <feynman.liang@gmail.com>
2016-09-05 20:29:16 -0700

    Adds semantic ui

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import MetreVisualizer from './MetreVisualizer';
+
+export default class App extends Component {
+  render() {
+    return (
+      <MetreVisualizer />
+    );
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -6,21 +6,47 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
-    './src/index'
+    './src/index.jsx'
   ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: path.join(__dirname, 'src'),
+        loaders: ['react-hot', 'babel-loader'],
+      },
+      { test: /\.css$/, loader: "style-loader!css-loader" },
+      {
+        test: /\.js$/,
+        include: /semantic/,
+        loaders: ['imports-loader?this=>window']
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|woff|woff2|ttf|eot)$/i,
+        loaders: ['file-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      'react': path.join(__dirname, 'node_modules', 'react')
+    },
+  },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.ProvidePlugin({
+      'jQuery': 'jquery',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'src')
-    }]
-  }
 };
